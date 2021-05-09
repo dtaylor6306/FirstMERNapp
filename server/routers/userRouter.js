c85d9084bc7dce5ae8bc2bf8f82664025a7c3ce7
Dylan Taylor <dtaylor6306@gmail.com>
2021-05-08 22:37:20 -0600

    The login/signup/logout router has now been completed

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,5 +1,9 @@
 const router = require("express").Router(); //sets up router built into express
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register
 router.post("/", async (req, res)=>{
     try{
         const {email, password, passwordVerify} = req.body;
@@ -30,14 +34,94 @@ router.post("/", async (req, res)=>{
             return res.status(400).json(
             {errorMessage: "An account with this email already exits."}
             );
-        
         }
+        
+        // Hash the password
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
 
-    }
-    catch(err){
+        // now save the user to db
+
+        const newUser = new User({
+            email,passwordHash
+        });
+
+        const savedUser = await newUser.save();
+
+        // sign the token
+
+        const token =  jwt.sign({
+            user: savedUser._id
+        }, 
+        process.env.JWT_SECRET);
+        
+        // send the token via HTTP cookie
+
+        res.cookie("token", token, {
+            httpOnly: true
+        }).send();
+
+    }catch(err){
         console.error(err);
-        res.status(500).send;
+        res.status(500).send();
     }
 });
+
+// Log in
+
+router.post("/login", async (req, res) =>{
+    try{
+        const {email, password} = req.body;
+
+        //validate
+
+        if(!email || !password){
+            return res.status(400).json({
+                errorMessage: "Please enter all fields."
+            })
+        }
+        const existingUser = await User.findOne({email});
+        
+        if(!existingUser){
+            return res.status(401).json({
+                errorMessage: "Invalid email or password."
+            });
+        }
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
+        
+        if(!passwordCorrect){
+            return res.status(401).json({
+                errorMessage: "Invalid email or password."
+            });
+        }
+
+        //sign a token
+        const token = jwt.sign({
+            user: existingUser._id
+        },
+         process.env.JWT_SECRET
+        );
+
+        //send token in HTTP only cookie
+
+        res.cookie("token",token,{
+            httpOnly:true,
+        }).send();
+
+    }catch(err){
+        console.error(err);
+        res.status(500).send();
+    }
+
+});
+
+//logout
+
+router.get("/logout",(req, res) =>{
+    res.cookie("token", "",{
+        httpOnly:true,
+        expires: new Date(0)    //clears the cookie because its expired
+    }).send();
+})
 
 module.exports = router;
